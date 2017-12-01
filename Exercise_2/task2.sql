8913cabb95a42cb094d3eea3f7c021b861f1e69e
Peter Schwarz <pe.schwarz@hotmail.de>
2017-12-01 16:13:21 +0100

    add new group by statement to remove duplicates (=same diagnosis on same patient in same year)

--- Exercise_2/task2.sql
+++ Exercise_2/task2.sql
@@ -3,12 +3,14 @@ FROM
   (SELECT diagnosis.ICD9CODE as firstICD9, transcript.VISITYEAR
     FROM "Diagnosis" as diagnosis
     INNER JOIN "Transcript" as transcript
-    ON diagnosis.PATIENTGUID = transcript.PATIENTGUID) as first
+    ON diagnosis.PATIENTGUID = transcript.PATIENTGUID
+    GROUP BY diagnosis.ICD9CODE, diagnosis.PATIENTGUID, transcript.VISITYEAR) as first
 INNER JOIN
   (SELECT diagnosis.ICD9CODE as secondICD9, transcript.VISITYEAR
     FROM "Diagnosis" as diagnosis
     INNER JOIN "Transcript" as transcript
-    ON diagnosis.PATIENTGUID=transcript.PATIENTGUID) as second
+    ON diagnosis.PATIENTGUID = transcript.PATIENTGUID
+    GROUP BY diagnosis.ICD9CODE, diagnosis.PATIENTGUID, transcript.VISITYEAR) as second
 ON firstICD9 < secondICD9 AND first.VISITYEAR = second.VISITYEAR
 GROUP BY firstICD9, secondICD9
 ORDER BY "Appearences" DESC
