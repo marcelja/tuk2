643802fec033ccb6724dbd71a8d28bc8b320bb52
Marcel Jankrift <marcel@jankrift.de>
2018-02-15 10:35:42 +0100

    update sql stmt

--- hackathon/app.py
+++ hackathon/app.py
@@ -19,7 +19,9 @@ def execute_stmt(stmt):
     cursor.execute(stmt)
     result_json = {}
     for row in cursor.fetchall():
-        result_json[row[0]] = float(row[1])
+        if row[0] not in result_json:
+            result_json[row[0]] = []
+        result_json[row[0]].append(float(row[1]))
     return jsonify(result_json)
 
 
@@ -45,7 +47,7 @@ def root():
 @app.route('/sql_statement/<key>')
 def sql_statement(key):
     print('Statement for: ' + key)
-    return execute_stmt('SELECT state,"C/C" FROM "TUKGRP4"."DEPPR"')
+    return execute_stmt('select t1.state, t1.c/t2.c, t1.year from (select count(*) as c,year,state from transformed_brfss where ADDEPEV2=1 and year!=2017 group by year,state) t1, (select count(*) as c,year,state from transformed_brfss where (ADDEPEV2=2 or addepev2=1) and year!=2017 group by year,state) t2 where t1.year=t2.year and t1.state=t2.state order by t1.state, t1.year')
     # if key in statements:
     #     return execute_stmt(statements[key].format(''))
     # else:
